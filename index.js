458ae0423e084eaa8e5d699b7a6348e82ce6ba8c
saranya <selvaraj.umasaranya@gmail.com>
2022-04-14 19:24:56 -0400

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const movieEl = document.getElementById('movie-input')
 const formEl = document.getElementById('form-el')
 const movieContent = document.getElementById('movie-content')
 
-
+let selectedMovie = []
 let movieList = [];
 let existingMovie = JSON.parse(localStorage.getItem('movieList'))
 if(existingMovie){
@@ -17,7 +17,6 @@ formEl.addEventListener('submit', async function(e) {
     e.preventDefault();
     let response = await fetch(`http://www.omdbapi.com/?s=${movieEl.value}&i=tt3896198&apikey=526a9387`)
     let data = await response.json();
-    console.log(data.Search)
     let searchList = data.Search.map((item) => {
          return item.imdbID
     })
@@ -27,7 +26,8 @@ formEl.addEventListener('submit', async function(e) {
         .then(response => response.json())
         .then(data => {
            result.push(data)
-           console.log(result)
+           selectedMovie= [...result]
+           console.log(selectedMovie)
            let html = result.map((movie) => {
                return `<div class="movie-container">
                <div class="poster">
@@ -38,7 +38,7 @@ formEl.addEventListener('submit', async function(e) {
                    <div class="movie-details">
                        <p>${movie.Runtime}</p>
                        <p>${movie.Genre}</p>
-                       <p><button class="action-btn" id="watchlist-btn"><i class="fa fa-plus-circle" aria-hidden="true"></i>WatchList</button></p>
+                       <p><button class="action-btn" onclick="addMovielist('${movie.imdbID}')" id="watchlist-btn"><i class="fa fa-plus-circle" aria-hidden="true"></i>WatchList</button></p>
                    </div>
                    <p class="plot">${movie.Plot}</p>
                </div>
@@ -47,30 +47,13 @@ formEl.addEventListener('submit', async function(e) {
            movieContent.innerHTML = html
         }
         )
-    })
-//     movieContent.innerHTML = `<div class="movie-container">
-//     <div class="poster">
-//         <img class="img-movie" src=${data.Poster} alt=""/>
-//     </div>
-//     <div class="movie-info">
-//         <h2>${data.Title} <span class="rating"><i id="star" class="fa fa-star" aria-hidden="true"></i>${data.imdbRating}</span></h2>
-//         <div class="movie-details">
-//             <p>${data.Runtime}</p>
-//             <p>${data.Genre}</p>
-//             <p><button class="action-btn" id="watchlist-btn"><i class="fa fa-plus-circle" aria-hidden="true"></i>WatchList</button></p>
-//         </div>
-//         <p class="plot">${data.Plot}</p>
-//     </div>
-// </div>`
-//     const watchlistBtn = document.getElementById('watchlist-btn')
-//     watchlistBtn.addEventListener('click', () => {
-//         addMovielist(data)
-//      })
-     
+    })     
 })
 
-function addMovielist(data) {
-    movieList.push(data)
+function addMovielist(movieId) {
+    let savedMovie = selectedMovie.find(movie => movie.imdbID === movieId)
+    console.log(savedMovie)
+    movieList.push(savedMovie)
     console.log(movieList)
     localStorage.setItem('movieList', JSON.stringify(movieList))
 }
